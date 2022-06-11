72af84c321b62f9c357b6f1d13721444819a343a
Bình Đặng <dangngocbinh.dnb@gmail.com>
2022-06-11 09:44:01 +0700

    Update install-certbot.sh

--- install-certbot.sh
+++ install-certbot.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-echo "install epel"
+echo ">>> remove old certbot"
+yum remove -y certbot
+echo ">>> install epel"
 sudo yum install -y epel-release
-echo "install snapd"
+echo ">>> install snapd"
 sudo yum install -y snapd
 
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
-echo "install core"
+echo ">>> install core"
 sudo snap install core; sudo snap refresh core
 sleep 5
-echo "install core again"
+echo ">>> install core again"
 sudo snap install core; sudo snap refresh core
-echo "install certbot"
+echo ">>> install certbot"
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-echo "done!"
+echo ">>> done!"
